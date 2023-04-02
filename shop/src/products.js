ae75873b57ae8a9b0cbd427a4d43bb6b9318fbf5
othmanedendane <othmanedendane99@gmail.com>
2023-04-02 13:18:04 -0400

    Initial commit

---
+++ shop/src/products.js
@@ -0,0 +1,87 @@
+import product1 from "./assets/red.png";
+import product2 from "./assets/war.webp";
+import product3 from "./assets/fifa.png";
+import product4 from "./assets/assassins.webp";
+import product5 from "./assets/samurai.png";
+import product6 from "./assets/katana.webp";
+import product7 from "./assets/ps4.webp";
+import product8 from "./assets/ps5.png";
+import product9 from "./assets/pc.png";
+import product10 from "./assets/clavier.png";
+import product11 from "./assets/casque.png";
+import product12 from "./assets/souris.png";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Red Dead Redemption II",
+        price: 79.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "God of war Ragnarok",
+        price: 75.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Fifa 23",
+        price: 89.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Assassin's Creed IV Black Flag",
+        price: 39.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Ghost of Tsushima",
+        price: 70.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Katana",
+        price: 489.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Play Station 4",
+        price: 399.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Play Station 5",
+        price: 899.0,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: "Pc gamer core i5 8th Gen 16Go Ram 256Go SSD 1To HDD",
+        price: 1100.0,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: "razer blackwidow v3 tenkeyless",
+        price: 139.0,
+        productImage: product10,
+    },
+    {
+        id: 11,
+        productName: "razer kraken x",
+        price: 87.0,
+        productImage: product11,
+    },
+    {
+        id: 12,
+        productName: "razer Naga Trinity",
+        price: 105.0,
+        productImage: product12,
+    },
+];
